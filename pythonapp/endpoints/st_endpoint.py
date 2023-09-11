25d928fdfd6bc0d6df65de346f8381ddc609fde8
Dan Greenberg <daniel.greenberg@excella.com>
2023-09-11 12:40:54 -0400

    python and react app updates

--- pythonapp/endpoints/st_endpoint.py
+++ pythonapp/endpoints/st_endpoint.py
@@ -1,10 +1,12 @@
 from flask import Flask, request, jsonify, json
 from flask_restful import Resource, Api, reqparse
+from flask_cors import CORS
 import pandas as pd
 import ast
 import military_symbol
 
 app = Flask(__name__)
+CORS(app)
 api = Api(app)
 
 
--- reactapp/src/App.js
+++ reactapp/src/App.js
@@ -1,24 +1,43 @@
 import logo from "./logo.svg";
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
 
 function App() {
+  const [inputs, setInputs] = useState({});
+  const [img, setImg] = useState("");
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    fetch(`http://18.189.126.187:8080/st?sidc=${inputs.sidc}`)
+      .then((response) => response.json())
+      .then((data) => setImg(data))
+      .catch((err) => {
+        console.log(err.message);
+      });
+  };
+
+  const handleChange = (event) => {
+    const name = event.target.name;
+    const value = event.target.value;
+    setInputs((values) => ({ ...values, [name]: value }));
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <form onSubmit={handleSubmit}>
+        <label>
+          Enter SIDC:
+          <input
+            type="text"
+            name="sidc"
+            value={inputs.sidc || ""}
+            onChange={handleChange}
+          />
+        </label>
+        <input type="submit" />
+      </form>
+      {img !== "" && (
+        <img src={`data:image/svg+xml;utf8,${encodeURIComponent(img)}`} />
+      )}
     </div>
   );
 }
